65613c0789f3c25be9ae4343fec4b89785528f95
arijitbhowal <arijitaeiou2002@gmail.com>
2023-09-07 20:04:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
+import React, {useRef} from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as handpose from '@tensorflow-models/handpose';
+import Webcam from 'react-webcam';
 import './App.css';
+import { drawHand } from './utilities';
 
 function App() {
+  const webcamRef=useRef(null);
+  const canvasRef=useRef(null);
+
+  const runHandPose=async ()=>{
+    const net=await handpose.load()
+    console.log("HandPose model is loaded")
+
+    setInterval(()=>{
+      detect(net)
+    },100)
+  };
+  const detect= async(net)=>{
+    if(typeof webcamRef.current != "undefined" && 
+    webcamRef.current != null &&
+    webcamRef.current.video.readyState ===4){
+      const video=webcamRef.current.video;
+      const videoWidth= webcamRef.current.video.videoWidth;
+      const videoHeight =webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.Width=videoWidth;
+      webcamRef.current.video.Height=videoHeight;
+
+      canvasRef.current.width=videoWidth;
+      canvasRef.current.height=videoHeight;
+
+      const hand = await net.estimateHands(video);
+      console.log(hand);
+
+      const ctx= canvasRef.current.getContext("2d");
+      drawHand(hand,ctx);
+    }
+  }
+  runHandPose();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Webcam
+        ref={webcamRef}
+        style={
+          {
+            position:"absolute",
+            marginLeft:"auto",
+            marginRight:"auto",
+            left:0,
+            right:0,
+            textAlign:"center",
+            zIndex:9,
+            width:640,
+            height:480
+          }
+        }
+        />
+        <canvas
+        ref={canvasRef}
+        style={{
+          position:"absolute",
+          marginLeft:"auto",
+          marginRight:"auto",
+          left:0,
+          right:0,
+          textAlign:"center",
+          zIndex:9,
+          width:640,
+          height:480
+        }}
+        />
+        </header>
+        </div>
   );
 }
 
